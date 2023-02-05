bd9c64340fb59cd6b9bf33047d2b84890b1e5a15
TaerangLee <s22051@gsm.hs.kr>
2023-02-05 16:23:05 +0900

    문자열을 정수로 바꾸기기

--- level_1/x만큼 간격이 있는 n개의 숫자.js
+++ level_1/x만큼 간격이 있는 n개의 숫자.js
@@ -3,12 +3,19 @@ function solution(x, n) {
     let result = 0;
     let cnt = 1;
     
-    
     for(let i =0; i < n; i++){
         result = x * cnt;
         answer.push(result); 
-        cnt++;
-        
+        cnt++; 
+    }
+    return answer;
+}
+
+//2번째 풀이! 
+function solution(x, n) {
+    let answer = [];    
+    for(let i =1; i <= n; i++){   
+        answer.push(x * i);        
     }
     
     return answer;
---
+++ level_1/문자열을 정수로 바꾸기.js
@@ -0,0 +1,3 @@
+function solution(s) {
+    return Number(s); //Number가 숫자로 변환함.
+}
